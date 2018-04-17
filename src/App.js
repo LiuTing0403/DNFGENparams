42bad740f4c4dc25dfc2b1523bc9317a09d7680f
LiuTing0403 <liuting0403@gmail.com>
2018-04-17 22:15:28 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,90 @@
-import React, { Component } from 'react';
+import React, { PureComponent, Component } from 'react';
+import {Form, Text, RadioGroup, Radio, Field, NestedField} from 'react-form'
 import logo from './logo.svg';
 import './App.css';
+import {NumInput, RadioInput, TextInput} from './FormItems'
+import {data} from './form.json'
 
-class App extends Component {
+function createArray(size) {
+  const result = []
+  for (let i = 0 ; i < size ; i ++) {
+    result.push(i)
+  }
+  return result
+}
+
+class ArrayInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      values: []
+    }
+  }
+  render() {
+    const {size} = this.props
+    const arr = createArray(size)
+    return (
+    <Field field={this.props.field}>
+    {fieldApi => {
+      const {onChange, field, ...rest} = this.props
+      const {value, setValue} = fieldApi
+      return (
+        <div>
+          {arr.map((item, idx) => {
+            return <input
+                key={idx}
+                value={this.state.values[idx] || ''}
+                type="number"
+                onChange={e => {
+                  this.state.values[idx] = e.target.value
+                  setValue(this.state.values)
+                  if (onChange) {
+                    onChange(e.target.value, e)
+                  }
+                }}
+            />})}
+        </div>
+      )
+    }}
+    </Field>)
+  }
+}
+
+class App extends PureComponent {
+  handleSubmit(values) {
+    console.log(values)
+  }
   render() {
     return (
-      <form>
-        <label>
-          stopCondition:
-          <input type="text" name="stopCondition"/>
-        </label>
-      </form>
+      <Form onSubmit={this.handleSubmit} render={({submitForm}) => (
+        <form onSubmit={submitForm}>
+          <div className="formContainer">
+            {data.map(group => {
+              return <div style={{flex: 1}} key={group.group_name}>
+                <div className="groupName">{group.group_name}</div>
+                {group.inputs.map((item) => {
+                  return <div key={item.name}>
+                    {item.sub_group && <div>{item.sub_group}</div>}
+                    {item.type === 'radio' && <RadioGroup field={item.name}>
+                      {item.title}
+                      {item.values && item.values.map(v =>
+                      <div key={`${item.name}-${v}`}>
+                        <Radio id={`${item.name}-${v}`} value={v}/>
+                        <label htmlFor={`${item.name}-${v}`}>{v}</label>
+                      </div>)}
+                    </RadioGroup>}
+                    {item.type === 'text' || item.type === 'number' && <div>
+                      {item.title}
+                      <Text field={item.name} placeholder={item.title} type={item.type}/>
+                    </div>}
+                    {item.type === 'array' && <ArrayInput field={item.name} size={item.size.reduce((partial, value) => partial + value)}/>}
+                  </div>
+                })}
+              </div>
+            })}
+          </div>
+          <button type="submit">Submit</button>
+        </form>)}/>
     );
   }
 }
--- src/FormItems.js
+++ src/FormItems.js
@@ -1,12 +1,26 @@
 import React, {PureComponent} from 'react'
+import {Radio, RadioGroup} from 'react-form'
 
 export class NumInput extends PureComponent {
   render() {
     return (
       <div>
         <label>
-          {this.props.name}
+          {this.props.title}
           <input type="number" name={this.props.name}/>
+        </label>
+      </div>
+    )
+  }
+}
+
+export class TextInput extends PureComponent {
+  render() {
+    return (
+      <div>
+        <label>
+          {this.props.title}
+          <input type="text" name={this.props.name}/>
         </label>
       </div>
     )
@@ -30,12 +44,27 @@ export class Select extends PureComponent {
     return (
       <div>
         <label>
-          {this.props.name}
+          {this.props.title}
           <select name={this.props.name} value={this.state.value} onChange={this.handleChange}>
             {this.props.options.map(option => <option value={option}>{option}</option>)}
           </select>
         </label>
       </div>
+    )
+  }
+}
+
+export class RadioInput extends PureComponent {
+  render() {
+    return (
+      <RadioGroup field={this.props.name}>
+        {this.props.title}
+        {this.props.values && this.props.values.map(v =>
+        <div key={`${this.props.name}-${v}`}>
+          <Radio id={`${this.props.name}-${v}`} value={v}/>
+          <label htmlFor={`${this.props.name}-${v}`}>{v}</label>
+        </div>)}
+      </RadioGroup>
     )
   }
 }
